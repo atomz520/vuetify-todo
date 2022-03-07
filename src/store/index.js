97078da373b4f100ca2e154c8a82f89069f98903
Shalom Quek <shalomquek@gmail.com>
2022-03-08 02:11:57 +0800

    76 Search

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    search: null,
     tasks:[
       {
         id: 1,
@@ -31,6 +32,9 @@ export default new Vuex.Store({
     }
   },
   mutations: {
+    setSearch(state, value) {
+      state.search = value
+    },
     addTask(state, newTaskTitle) {
       let newTask = {
         id: Date.now(),
@@ -90,5 +94,13 @@ export default new Vuex.Store({
     }
   },
   modules: {
+  },
+  getters: {
+    tasksFiltered(state) {
+      if(!state.search) {
+        return state.tasks
+      }
+      return state.tasks.filter(task => task.title.toLowerCase().includes(state.search.toLowerCase()))
+    }
   }
 })
